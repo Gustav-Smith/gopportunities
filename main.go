59557741e4d4a9bde7f9082795e88ac980ee1ee0
Gustav-Smith <gustavojeanpt@gmail.com>
2023-12-09 13:53:11 -0300

    feat: add opening schema package

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/Gustav-Smith/gopportunities/router"
+import (
+	"github.com/Gustav-Smith/gopportunities/config"
+	"github.com/Gustav-Smith/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
-
+	logger = *config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v",err)
+		return
+	}
 	// Initialize Router
 	router.Initialize()
 }
