5da4d07072db5464e4dabecedd338d0273ec39de
Gustav-Smith <gustavojeanpt@gmail.com>
2023-12-09 15:58:53 -0300

    feat: add database handler

---
+++ config/sqlite.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"os"
+
+	"github.com/Gustav-Smith/gopportunities/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+	// Check if the database file exists
+	_, err := os.Stat("./db/main.db")
+	if os.IsNotExist(err){
+		logger.Info("database file not found, creating...")
+		// Create the database file and directory
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err !=nil{
+			return nil, err
+		}
+		file.Close()
+	}
+	// Create DB and connect
+	db, err := gorm.Open(sqlite.Open("./db/main.db"), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+	// Migrate the Schemas
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite automigration error: %v", err)
+		return nil, err
+	}
+	return db, nil
+}
